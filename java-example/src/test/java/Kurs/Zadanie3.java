d9fbc16dfa9723622e4dae398d427fbc0b99e9ed
agatanu <nurkiewiczagata@gmail.com>
2019-05-03 09:37:33 +0200

    Zadanie3

--- java-example/src/test/java/Kurs/Zadanie3.java
+++ java-example/src/test/java/Kurs/Zadanie3.java
@@ -3,26 +3,30 @@ package Kurs;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Zadanie1 {
+public class Zadanie3 {
 
     private WebDriver driver;
 
     @Before
     public void start() {
-        //System.setProperty("webdriver.chrome.driver","C:\\Users\\Agata\\Selenium\\Drivers\\chromedriver.exe");
         driver = new ChromeDriver();
     }
 
     @Test
     public void test(){
-        driver.get("http://facebook.com");
+        driver.get("http://localhost:5050/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.xpath("//button[@name='login']")).click();
     }
 
     @After
     public void stop(){
         driver.quit();
     }
+
 }
