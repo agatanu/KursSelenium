c4176c2699b5270a3f5ac3457962a8b6e4ae1557
agatanu <nurkiewiczagata@gmail.com>
2019-05-09 22:34:06 +0200

    created separate class for Zadanie7

--- java-example/src/test/java/Kurs/Zadanie3.java
+++ java-example/src/test/java/Kurs/Zadanie3.java
@@ -23,59 +23,15 @@ public class Zadanie3 {
         driver = new ChromeDriver();
         //driver = new FirefoxDriver();
         //driver = new InternetExplorerDriver();
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 
     }
-
+    @Test
     public void login() {
         driver.get("http://localhost:5050/litecart/admin/");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.xpath("//button[@name='login']")).click();
 
-    }
-
-    //TIME CONSUMING METHOD
-
-    @Test
-    public void checkMenuElements() {
-
-        login();
-
-        driver.findElement(By.cssSelector("#app-appearance")).click();
-        assertTrue(driver.findElement(By.cssSelector("h1")).isDisplayed());
-        driver.findElement(By.cssSelector("#doc-logotype")).click();
-        assertTrue(driver.findElement(By.cssSelector("h1")).isDisplayed());
-        // ...
-    }
-
-    //OPTIMISED METHOD
-    @Test
-    public void checkMenuElementsOptimised() {
-
-        List<WebElement> menuElements;
-        List<WebElement> submenuElements;
-
-        login();
-
-        menuElements = driver.findElements(By.cssSelector("[id^=app-]"));
-        for (int a = 0; a < menuElements.size(); a++) {
-            menuElements = driver.findElements(By.cssSelector("[id^=app-]"));
-            menuElements.get(a).click();
-            menuElements = driver.findElements(By.cssSelector("[id^=app-]"));
-            assertTrue(driver.findElement(By.cssSelector("h1")).isDisplayed());
-
-            if (driver.findElements(By.cssSelector(".docs")).size() > 0) {
-                submenuElements = driver.findElements(By.cssSelector("[id^=doc-]"));
-                for (int b = 0; b < submenuElements.size(); b++) {
-                    submenuElements = driver.findElements(By.cssSelector("[id^=doc-]"));
-                    submenuElements.get(b).click();
-                    assertTrue(driver.findElement(By.cssSelector("h1")).isDisplayed());
-
-                }
-            }
-
-        }
     }
 
     @After
