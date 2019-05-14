5ff7813402cdb272666d2401bf1df8b423b25076
agatanu <nurkiewiczagata@gmail.com>
2019-05-14 22:43:30 +0200

    whole exc.9 but with small problem in method "checkSortingForZonesInCountries"

--- java-example/src/test/java/Kurs/Zadanie9.java
+++ java-example/src/test/java/Kurs/Zadanie9.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static sun.security.jgss.GSSUtil.login;
 
 public class Zadanie9 {
@@ -27,18 +28,18 @@ public class Zadanie9 {
     }
 
     public void login() {
-        driver.get("http://localhost:5050/litecart/admin/");
+        driver.get("http://litecart.stqa.ru/admin/");
         driver.findElement(By.name("username")).sendKeys("admin");
-        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("0b7dba1c77df25bf0");
         driver.findElement(By.xpath("//button[@name='login']")).click();
     }
 
     @Test
-    public void checkAlphabeticalOrder(){
+    public void checkAlphabeticalOrder() {
 
         login();
 
-        driver.findElement(By.cssSelector("#app-countries")).click();
+        driver.findElement(By.xpath("//td[@id='sidebar']//li[3]//a[1]")).click();
         List<WebElement> countries = driver.findElements(By.xpath("//tr/td[5]/a[contains(@href,'countries')]"));
 
         List<String> listOfCountries = new ArrayList<String>();
@@ -47,17 +48,86 @@ public class Zadanie9 {
             listOfCountries.add(countries.get(i).getText());
         }
 
-        List<String> sortedListOfCountries = new ArrayList<String>(listOfCountries);
+
+        List<String> unsortedListOfCountries = new ArrayList<String>(listOfCountries);
         Collections.sort(listOfCountries);
 
-        if(listOfCountries.equals(sortedListOfCountries)){
+        if (listOfCountries.equals(unsortedListOfCountries)) {
             System.out.println("Alphabetical order is correct! PASSED!");
-        }
-        else {
+        } else {
             System.out.println("Alphabetical order is NOT correct! FAILED!");
         }
         //Uncomment the row below to see differences
         //assertEquals(listOfCountries,sortedListOfCountries);
+    }
+
+    @Test
+    public void checkSortingForZonesInCountries() {
+
+        login();
+
+        driver.findElement(By.xpath("//td[@id='sidebar']//li[3]//a[1]")).click();
+        int numberOfCountries = driver.findElements(By.cssSelector("tr.row")).size();
+
+        for (int i = 0; i < numberOfCountries; i++) {
+            List<WebElement> countries = driver.findElements(By.xpath("//tr/td[5]/a[contains(@href,'countries')]"));
+            int zonesCount = Integer.parseInt(driver.findElement(By.xpath("//tr/td[6]")).getText());
+
+            if (zonesCount > 0) {
+                countries.get(i).click();
+                //System.out.println(driver.getTitle());
+                int zonesSize = driver.findElements(By.xpath("//tr/td[3]")).size();
+                List<String> zonesList = new ArrayList<String>();
+
+                for (int j = 0; j < zonesSize; j++) {
+
+                    List<WebElement> zones = driver.findElements(By.xpath("//tr/td[3]"));
+                    zonesList.add(zones.get(j).getAttribute("value"));
+                }
+
+                driver.navigate().back();
+
+                List<String> zonesListBeforeSorting = new ArrayList<String>(zonesList);
+                Collections.sort(zonesList);
+                assertEquals(zonesList, zonesListBeforeSorting);
+            }
+
+        }
+
+    }
+
+    @Test
+    public void checkSortingForZonesInCountriesFromGeoZones() {
+
+        login();
+
+        driver.findElement(By.xpath("//td[@id='sidebar']//li[6]//a[1]")).click();
+        int numberOfCountries = driver.findElements(By.cssSelector("tr.row")).size();
+
+        for (int i = 0; i < numberOfCountries; i++) {
+            List<WebElement> countries = driver.findElements(By.xpath("//tr/td[3]/a[contains(@href,'geo_zones')]"));
+            int zonesCount = Integer.parseInt(driver.findElement((By.xpath("//tr/td[4]"))).getText());
+
+            if (zonesCount > 0) {
+                countries.get(i).getText();
+                countries.get(i).click();
+                List<WebElement> geoZones = driver.findElements(By.xpath("//tr/td[3]"));
+                List<String> geoZonesList = new ArrayList<String>();
+
+                for (int j = 0; j < geoZones.size(); j++) {
+
+                    String geoZoneName = geoZones.get(j).findElement(By.xpath("//select[contains(@name,'zones')]")).getAttribute("textContent");
+                    geoZonesList.add(geoZoneName);
+                }
+
+                driver.navigate().back();
+
+                List<String> unsortedGeoZonesList = new ArrayList<String>(geoZonesList);
+                Collections.sort(geoZonesList);
+                assertEquals(geoZonesList, unsortedGeoZonesList);
+            }
+        }
+
     }
 
     @After
