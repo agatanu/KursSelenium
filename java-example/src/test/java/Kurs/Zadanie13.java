5323206997a889b9ea83e1454afe3c6b9db693f2
agatanu <nurkiewiczagata@gmail.com>
2019-05-25 20:39:19 +0200

    Zadanie13 fixes

--- java-example/src/test/java/Kurs/Zadanie13.java
+++ java-example/src/test/java/Kurs/Zadanie13.java
@@ -1,85 +1,62 @@
 package Kurs;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
 import java.util.concurrent.TimeUnit;
-
-import static org.junit.Assert.assertEquals;
-import static org.openqa.selenium.support.ui.ExpectedConditions.stalenessOf;
 
 public class Zadanie13 {
 
-    WebDriver driver;
-    WebDriverWait wait;
+    private WebDriver driver;
 
     @Before
     public void start() {
         driver = new ChromeDriver();
-        driver.get("http://localhost:5050/litecart");
+        driver.get("http://litecart.stqa.ru");
+        driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
     }
 
     @Test
-    public void addAndDeleteProducts() {
+    public void addAndRemoveProducts() {
+        //add (without loop)
+        int currentQuantityInCart = Integer.parseInt(driver.findElement(By.cssSelector("span.quantity")).getText());
 
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        wait = new WebDriverWait(driver, 3);
+        driver.findElement(By.xpath("//div[contains(text(),'Red Duck')]")).click();
+        driver.findElement(By.xpath("//button[@name='add_cart_product']")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 8);
+        wait.until(ExpectedConditions.textToBe(By.cssSelector("span.quantity"), String.valueOf(1 + currentQuantityInCart)));
+        driver.get("http://litecart.stqa.ru");
 
-        //add products
+        driver.findElement(By.xpath("//div[contains(text(),'Purple Duck')]")).click();
+        driver.findElement(By.xpath("//button[@name='add_cart_product']")).click();
+        wait.until(ExpectedConditions.textToBe(By.cssSelector("span.quantity"), String.valueOf(2 + currentQuantityInCart)));
+        driver.get("http://litecart.stqa.ru");
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'Popular Products')]"))).click();
+        driver.findElement(By.xpath("//div[contains(text(),'Green Duck')]")).click();
+        driver.findElement(By.xpath("//button[@name='add_cart_product']")).click();
+        wait.until(ExpectedConditions.textToBe(By.cssSelector("span.quantity"), String.valueOf(3 + currentQuantityInCart)));
+        driver.get("http://litecart.stqa.ru");
 
-        WebElement redDuck = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(),'Red Duck')]")));
-        redDuck.click();
-        WebElement addToCartButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[name='add_cart_product']")));
-        addToCartButton.click();
-        driver.findElement(By.xpath("//div[@class='featherlight-close-icon featherlight-close']")).click();
-        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@class='featherlight active']")));
+        driver.get("http://litecart.stqa.ru/en/checkout");
 
-
-        WebElement blueDuck = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(),'Blue Duck')]")));
-        blueDuck.click();
-        wait.until(ExpectedConditions.visibilityOf(addToCartButton));
-        addToCartButton.click();
-        driver.findElement(By.xpath("//div[@class='featherlight-close-icon featherlight-close']")).click();
-        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@class='featherlight active']")));
-
-
-        WebElement purpleDuck = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(),'Purple Duck')]")));
-        purpleDuck.click();
-        wait.until(ExpectedConditions.visibilityOf(addToCartButton));
-        addToCartButton.click();
-        driver.findElement(By.xpath("//div[@class='featherlight-close-icon featherlight-close']")).click();
-        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@class='featherlight active']")));
-
-
-        //remove products
-        WebElement cart = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='cart']//a")));
-        cart.click();
-
-        driver.findElement(By.xpath("//tr[1]//td[6]//button[1]")).click();
-        WebElement summaryTable = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#box-checkout-summary")));
-        driver.findElement(By.xpath("//tr[1]//td[6]//button[1]")).click();
-        wait.until(ExpectedConditions.visibilityOf(summaryTable));
-        driver.findElement(By.xpath("//button[@name='remove_cart_item']")).click();
-        wait.until(ExpectedConditions.invisibilityOf(summaryTable));
-
+        //remove (with loop)
+        for (int a = 0; a <= 2; a++) {
+            driver.findElement(By.xpath("//button[@name='remove_cart_item']")).click();
+            WebElement summaryTable = driver.findElement(By.xpath("//div[@id='box-checkout-summary']"));
+            wait.until(ExpectedConditions.stalenessOf(summaryTable));
+        }
     }
-
 
     @After
     public void after() {
         driver.quit();
     }
-
 }
+
