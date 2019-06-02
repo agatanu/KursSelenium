c44d6126f3d76cfa4b967dcea0ee8a7669b5f9ab
agatanu <nurkiewiczagata@gmail.com>
2019-06-02 10:45:52 +0200

    Zadanie16 browser stack

--- java-example/src/test/java/Kurs/Zadanie15.java
+++ java-example/src/test/java/Kurs/Zadanie15.java
@@ -1,21 +1,33 @@
 package Kurs;
 
-import org.openqa.selenium.Capabilities;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
 import java.net.URL;
-
-import static org.openqa.selenium.remote.DesiredCapabilities.*;
+import java.rmi.Remote;
 
 public class Zadanie15 {
 
     WebDriver driver;
 
-    public void virtualBoxTest() {
-        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),DesiredCapabilities.chrome());
-        chrome.get("https://www.google.com");
+    @Test
+    public void virtualBoxTest() throws MalformedURLException {
+
+        ChromeOptions options = new ChromeOptions();
+        new ChromeDriver(options);
+
+        driver = new RemoteWebDriver(new URL("http://10.0.2.15:4444/wd/hub"), options);
+        driver.get("https://www.google.com");
+
+        driver.quit();
+
+//       On separate terminals in virtual machine:
+//        java -jar selenium-server-standalone-3.141.59.jar -role hub
+//        java -jar selenium-server-standalone-3.141.59.jar -role node -hub http://192.168.56.1:4444/wd/hub
     }
 }
